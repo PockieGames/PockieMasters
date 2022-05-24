f5547d1045e85b101397b883b23581e2dd9b55c0
Sahin Habermann <sahyno1996@gmail.com>
2022-05-25 00:17:37 +0200

    Fix: Import

--- Client/assets/scripts/battle/tileObjects/TileAttackableObject.ts
+++ Client/assets/scripts/battle/tileObjects/TileAttackableObject.ts
@@ -1,5 +1,5 @@
 import AttackableObject from "../../shared/game/battle/stats/AttackableObject";
-import { TileObject } from "../TileObject";
+import { TileObject } from "./TileObject";
 
 export default class TileAttackableObject extends TileObject{
 
