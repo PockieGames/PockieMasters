1c201ec1135bd52da2fffef918f0398449d45d5a
Sahin Habermann <sahyno1996@gmail.com>
2022-06-15 00:22:46 +0200

    Add: Switch View to Home UI

--- Client/assets/scripts/scene/HomeScene.ts
+++ Client/assets/scripts/scene/HomeScene.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Button } from 'cc';
 import UIManager from '../ui/UIManager';
 import HomeUI from '../ui/views/HomeUI';
 const { ccclass, property } = _decorator;
@@ -9,12 +9,38 @@ export class HomeScene extends Component {
     @property(Node)
     viewNode: Node
 
-    async start() {
-        //let homeUI = await UIManager.Instance<UIManager>().OpenUI(HomeUI)
+    @property(Prefab)
+    chapterPrefab: Prefab
+    @property(Button)
+    chapterBtn: Button
+    @property(Prefab)
+    shopPrefab: Prefab
+    @property(Button)
+    shopButton: Button
+
+    start() {
+        this.replaceView(this.chapterPrefab)
+
+        this.chapterBtn.node.on(Button.EventType.CLICK, () => {
+            this.replaceView(this.chapterPrefab)
+        })
+
+        this.shopButton.node.on(Button.EventType.CLICK, () => {
+            this.replaceView(this.shopPrefab)
+        })
+    }
+
+    replaceView(prefab: Prefab) {
+        let newNode = instantiate(prefab)
+        if (this.viewNode.children.length > 0) {
+            let oldNode = this.viewNode.children[0]
+            oldNode.destroy()
+        }
+        newNode.setParent(this.viewNode)
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
 
