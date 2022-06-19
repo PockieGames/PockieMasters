21b17c3c4fda3a1d18706d9463a63e51d209fd36
Sahin Habermann <sahyno1996@gmail.com>
2022-06-19 22:57:17 +0200

    Modify: StoryUI

--- Client/assets/scripts/ui/views/StoryUI.ts
+++ Client/assets/scripts/ui/views/StoryUI.ts
@@ -1,14 +1,29 @@
 import { _decorator, Component, Node, Button, director } from 'cc';
+import { AnimatedRichText } from '../../utils/AnimatedRichText';
 import UIBase from '../UIBase';
 const { ccclass, property } = _decorator;
 
 @ccclass('StoryUI')
 export class StoryUI extends UIBase {
+
+    @property({type: AnimatedRichText})
+    animatedDialogueText: AnimatedRichText
+
     @property({type: Button})
     skipBtn: Button
 
     start() {
+        this.animatedDialogueText.onFinish = () => {
+            // Wait 3 Seconds before loading
+            this.scheduleOnce(() => {
+                director.loadScene("home")
+            }, 3)
+        }
+        this.animatedDialogueText.node.on(Node.EventType.MOUSE_UP, () => {
+            this.animatedDialogueText.finishNow()
+        })
         this.skipBtn.node.on(Button.EventType.CLICK, async () => {
+            this.unscheduleAllCallbacks()
             director.loadScene("home");
         })
     }
