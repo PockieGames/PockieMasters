a7d7f12b16df3f240f8a6d6670cc14c4572df31f
Sahin Habermann <sahyno1996@gmail.com>
2022-06-20 02:10:36 +0200

    Modify: HeroesUI

--- Client/assets/scripts/ui/views/HeroesUI.ts
+++ Client/assets/scripts/ui/views/HeroesUI.ts
@@ -1,10 +1,14 @@
-import { Component, instantiate, Node, Prefab, _decorator } from "cc";
+import { Component, dragonBones, instantiate, Node, Prefab, _decorator } from "cc";
 import UserManager from "../../manager/UserManager";
+import HeroData from "../../shared/game/data/HeroData";
 
 const { ccclass, property } = _decorator;
 
 @ccclass("HeroesUI")
 export default class HeroesUI extends Component{
+
+    @property(dragonBones.ArmatureDisplay)
+    dragonBonesComponent: dragonBones.ArmatureDisplay
 
     @property(Node)
     scrollViewContent: Node
@@ -13,9 +17,12 @@ export default class HeroesUI extends Component{
     heroFramePrefab: Prefab
 
     start(){
-        UserManager.Instance<UserManager>().heroes.forEach((heroData) => {
+        UserManager.Instance<UserManager>().heroes.forEach((heroData: HeroData) => {
             let heroFrame = instantiate(this.heroFramePrefab)
             heroFrame.setParent(this.scrollViewContent)
+            heroFrame.on(Node.EventType.MOUSE_UP || Node.EventType.TOUCH_END, () => {
+                heroData.sprite
+            })
         })
     }
 
